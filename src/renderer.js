2d6759c96e65cbc33dfcfe667034ae77c47c3216
juandozuna <juandanielozuna2@gmail.com>
2018-04-20 12:17:23 -0400

    Popper and jQuery added correctly

--- src/renderer.js
+++ src/renderer.js
@@ -1,4 +1,9 @@
 import Vue from 'vue';
 import App from './app';
 
+let $ = require('jquery');
+require('bootstrap');
+require('popper.js');
+require('materialize-css');
+
 const app = new Vue(App).$mount('#app');
